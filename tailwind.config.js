4fbeec24c518a3a857841c4daba23a7e93f5d49c
Dmitriy <aua008@gmail.com>
2025-02-05 15:17:44 +0200

    updated tailwind to v4, added new dashboard elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,222 +15,9 @@ const hexToRgb = hex => {
 module.exports = {
 	darkMode: 'selector',
 	content: ['./**/*.{html,js}'],
-	theme: {
-		container: {
-			center: true,
-			padding: '20px',
-			screens: {
-				xl: '1200px',
-			},
-		},
-		colors: {
-			white: colors.white,
-			black: colors.black,
-			yellow: {
-				400: '#DDB51E',
-				500: '#FBC045',
-				600: '#FFBC03',
-				700: '#C88E10',
-			},
-			gray: {
-				100: '#FBFBFD',
-				200: '#F8F9FC',
-				300: '#f1f2f5',
-				400: '#E8E9ED',
-				500: '#D9DEE7',
-				600: '#D3D6DC',
-				700: '#999cb2',
-				800: '#59657E',
-			},
-			orange: {
-				100: '#faf6f3',
-				500: '#FE6E2B',
-				950: '#1e0c04',
-			},
-			red: {
-				500: '#E61320',
-			},
-			blue: {
-				50: '#E9F1F6',
-				75: '#F5F9FA',
-				100: '#f2f6ff',
-				400: '#09B4DC',
-				500: '#2F76FF',
-				600: '#4C78EA',
-				700: '#1c5ddf',
-				800: '#082A35',
-				900: '#122345',
-				950: '#130c3e',
-				1000: '#091329',
-			},
-			violet: {
-				300: '#9489B6',
-				400: '#9F99B2',
-				500: '#6259EB',
-				600: '#9452FF',
-				700: '#27293D',
-				800: '#1E1D33',
-				850: '#181729',
-				900: '#060515',
-			},
-			green: {
-				25: '#D1FAE5',
-				400: '#429D17',
-				500: '#047857',
-			},
-		},
-		fontFamily: {
-			sans: ['Inter', ...defaultTheme.fontFamily.sans],
-			'sans-semi': ['GeneralSans-semibold', ...defaultTheme.fontFamily.sans],
-			'sans-variable': [
-				'GeneralSans-Variable',
-				...defaultTheme.fontFamily.sans,
-			],
-			'sans-medium': ['GeneralSans-medium', ...defaultTheme.fontFamily.sans],
-		},
-		extend: {
-			screens: {
-				xxl: '1360px',
-				md: '790px', // phones
-				xs: '480px', // phones
-			},
-			animation: {
-				fade: 'fadeIn .3s ease forwards',
-				fadeOut: 'fadeOut .3s ease-in-out forwards',
-				fadeScaleIn: 'fadeScaleIn .8s ease-in-out forwards',
-				rotateUp: 'rotateUp .5s ease forwards',
-				slideUp: 'slideUp .9s ease forwards',
-			},
-			keyframes: {
-				fadeIn: {
-					from: { opacity: 0 },
-					to: { opacity: 1 },
-				},
-				fadeOut: {
-					from: { opacity: 1 },
-					to: { opacity: 0 },
-				},
-				fadeScaleIn: {
-					from: { opacity: 0, transform: 'scale(1.05)' },
-					to: { opacity: 1, transform: 'scale(1)' },
-				},
-				rotateUp: {
-					from: { marginBottom: '-80px', transform: 'rotate(5deg)' },
-					to: { marginBottom: '0px', transform: 'rotate(0deg)' },
-				},
-				slideUp: {
-					from: { opacity: 0, transform: 'translateY(20px)' },
-					to: { opacity: 1, transform: 'translateY(0)' },
-				},
-			},
-			ringWidth: {
-				2.5: '2.5px',
-			},
-			fontSize: {
-				'6xl': ['3rem', '3.75rem'], // 48px 60px
-				'4.5xl': ['2.5rem', '125%'],
-				'2.5xl': ['1.75rem', '2.45rem'],
-				base: ['1rem', '1.42rem'],
-				lg: ['1.125rem', '1.6875rem'], // 18px
-				sm: ['0.9375rem', '1.25rem'],
-				m: ['0.9375rem', '1.5rem'],
-				s: ['0.8125rem', '1.0562rem'],
-			},
-			lineHeight: {
-				5.5: '1.42rem', // 22.72px
-				5.8: '1.5rem', // 24px
-				6: '1.575rem', // 25.2px
-				6.5: '1.6875rem', // 27px
-				9.5: '2.4rem', // 38.4px
-			},
-			zIndex: {
-				1: '1',
-			},
-			backgroundPosition: {
-				'center-top': 'center top',
-			},
-			letterSpacing: {
-				0.4: '-0.4px',
-				0.22: '-0.22px',
-				normal: '-0.22px',
-				base: 'normal',
-				0.56: '-0.56px',
-				1.44: '-1.44px',
-			},
-			borderRadius: {
-				'2.5xl': '20px',
-			},
-			boxShadow: {
-				'inset-3xl':
-					'inset 0px -0.876389px 0.876389px 0.876389px rgba(17, 34, 69, 0.12)',
-				card: '0 2px 8px -1px #130c3e0a, 0 0 0 1px #130c3e1c, 0 4px 4px -5px #130c3e40;',
-				violet:
-					'0 1px 2px 0 rgba(31, 18, 69, 0.08), 0 0 0 1px rgba(58, 22, 202, 0.75), 0 2px 4.3px -1px rgba(58, 22, 202, 0.4)',
-				'card-use-cases': '0px 5px 30.9px -4px rgba(58, 68, 89, 0.27)',
-				'card-dark': '0px 1px 7px rgba(15, 28, 53, 0.04)',
-				'card-dark-contact':
-					'0px 2.85px 18.335px 3.8px rgba(0, 0, 0, 0.1), inset 0px 0px 0px 1.02317px rgba(255, 255, 255, 0.04)',
-				'card-dark-menu':
-					'0px 11.4701px 23.7768px -5.01619px rgba(255, 255, 255, 0.07), 0px 2px 7px -1px rgba(255, 255, 255, 0.04), inset 0px 0px 0px 1px rgba(255, 255, 255, 0.12)',
-				'card-orange':
-					'0px 4px 4px -5px rgba(30, 12, 4, 0.25), 0px 0px 0px 1px rgba(34, 14, 5, 0.08), 0px 1px 7px rgba(30, 12, 4, 0.04)',
-				'card-white':
-					'0px 1px 6.3px -1px rgba(18, 35, 69, 0.1), 0px 0px 0px 1px rgba(18, 35, 69, 0.075)',
-				img: '0 23.8px 30.3px 0 #0f1c3511, 0 0 0 0.8px #12234513, 0 0.8px 5px -0.8px #12234519, 0 3.2px 3.2px -4px #12234540, 0 0.8px 1.6px 0 #1223450f, 0 -2px 20.6px 0 #0000000c',
-				banner:
-					'0px 1px 3px rgba(18, 35, 69, 0.1), 0px 1px 2px rgba(18, 35, 69, 0.06), inset 0px 0px 0px 1px rgba(18, 35, 69, 0.1)',
-				social:
-					' 0px 6px 4px -2px rgba(18, 35, 69, 0.03), 0px 5.01227px 40.0982px -1px rgba(100, 109, 134, 0.06), 0px 1px 2px rgba(18, 35, 69, 0.05), 0px 0px 0px 1px rgba(217, 222, 231, 0.4)',
-				'menu-item':
-					'0px 2px 7px -1px rgba(15, 28, 53, 0.04),inset 0px 3px 0px -1px rgba(255, 255, 255, 0.46),inset 0px 0px 0px 1px rgba(18, 35, 69, 0.1),0px 11.47px 23.78px -5.02px rgba(18, 35, 69, 0.07)',
-				video:
-					'0px 11.4701px 23.7768px -5.01619px rgba(18, 35, 69, 0.07), inset 0px -0.866597px 0.866597px 0.866597px rgba(17, 34, 69, 0.12)',
-				avatar:
-					'0 7.3px 12.2px -3.8px rgba(18, 35, 69, 0.2), 0 16.4px 35.9px -4.9px rgba(18, 35, 69, 0.09)',
-			},
-			dropShadow: {
-				'3xl':
-					'drop-shadow(0px 0.876389px 7.01111px rgba(13, 34, 71, 0.08)) drop-shadow(0px -1.85479px 19.4753px rgba(0, 0, 0, 0.05))',
-			},
-			spacing: {
-				4.5: '1.125rem', // 18px
-				6.5: '1.625rem', // 26px
-				7.5: '1.875rem', // 30px
-				9.5: '2.375rem', // 38px
-				13: '3.25rem', // 52px
-				15: '3.75rem', // 60px
-				15.5: '3.875rem', // 62px
-				16.5: '4.25rem', // 68px
-				17: '4.5rem', // 72px
-				17.5: '4.6875rem', // 75px
-				22: '5.625rem', // 90px
-				25: '105px', // 105px
-				30: '7.5625rem', // 121px
-				34: '8.5rem', // 136px
-			},
-			backgroundImage: {
-				main: "url('/assets/images/main-bg.png')",
-				'main-ai': "url('/assets/images/features/ai/main-bg.png')",
-				'card-right': "url('/assets/images/bg-card-right.png')",
-				'card-left-orange': "url('/assets/images/card-bg-orange.png')",
-				'card-right-blue': "url('/assets/images/card-bg-blue.png')",
-				'card-right-vivid-blue':
-					"url('/assets/images/bg-card-right-vivid-blue.png')",
-				'video-ai': "url('/assets/images/video-ai-bg.png')",
-				'card-dark-center': "url('/assets/images/bg-card-dark.png')",
-				'banner-round': "url('/assets/images/banner-bg.png')",
-				// 'main-card-gradient': "url('/assets/images/bg-card-blue.png')",
-			},
-			opacity: {
-				8: '0.08',
-			},
-		},
-	},
 	plugins: [
-		require('tailwindcss'),
-		require('autoprefixer'),
 		require('tailwind-hamburgers'),
-		plugin(function ({ addComponents, addBase, theme }) {
+		plugin(function ({ addComponents, theme }) {
 			addComponents({
 				'.list-with-circle-check': {
 					li: {
@@ -625,62 +412,218 @@ module.exports = {
 						lineHeight: '1.7rem !important',
 					},
 				},
-			}),
-				addBase({
-					h1: {
-						fontFamily: theme('fontFamily.sans-variable'),
-						fontSize: '3.25rem', // 52px
-						fontWeight: 600,
-						lineHeight: '3.81rem',
-						letterSpacing: '-1.56px',
-						color: theme('colors.blue[900]'),
-						textAlign: 'left',
-						'@media not all and (min-width: 480px)': {
-							fontSize: '2.25rem',
-							lineHeight: '2.6437rem',
-							textAlign: 'center',
-						},
-					},
-					h2: {
-						fontFamily: theme('fontFamily.sans-variable'),
-						fontSize: '3rem', // 48px
-						textAlign: 'center',
-						fontWeight: 600,
-						fontStretch: 'normal',
-						fontStyle: 'normal',
-						lineHeight: '3.75rem', // 60px
-						letterSpacing: '-1.44px',
-						color: theme('colors.blue[950]'),
-						'@media not all and (min-width: 640px)': {
-							fontSize: '2.25rem',
-							lineHeight: '2.8125rem',
-						},
-					},
-					h3: {
-						fontFamily: theme('fontFamily.sans-variable'),
-						fontSize: '2rem', // 32px
-						fontWeight: 600,
-						lineHeight: '2.4rem',
-						letterSpacing: '-0.22px',
-						color: theme('colors.blue[950]'),
-						'@media not all and (min-width: 640px)': {
-							fontSize: '1.4rem',
-							lineHeight: '1.7rem !important',
-						},
-					},
-					h4: {
-						fontSize: '1.375rem',
-						letterSpacing: '-0.22px',
-						lineHeight: '1.65rem',
-						fontWeight: 600,
-						fontFamily: theme('fontFamily.sans-variable'),
-						color: theme('colors.blue[950]'),
-						'@media not all and (min-width: 640px)': {
-							fontSize: '1rem',
-							lineHeight: '1.3rem',
-						},
-					},
-				})
+			})
 		}),
 	],
+	theme: {
+		// container: {
+		// 	center: true,
+		// 	padding: '20px',
+		// 	screens: {
+		// 		xl: '1200px',
+		// 	},
+		// },
+		// colors: {
+		// 	white: colors.white,
+		// 	black: colors.black,
+		// 	yellow: {
+		// 		400: '#DDB51E',
+		// 		500: '#FBC045',
+		// 		600: '#FFBC03',
+		// 		700: '#C88E10',
+		// 	},
+		// 	gray: {
+		// 		100: '#FBFBFD',
+		// 		200: '#F8F9FC',
+		// 		300: '#f1f2f5',
+		// 		400: '#E8E9ED',
+		// 		500: '#D9DEE7',
+		// 		600: '#D3D6DC',
+		// 		700: '#999cb2',
+		// 		800: '#59657E',
+		// 	},
+		// 	orange: {
+		// 		100: '#faf6f3',
+		// 		500: '#FE6E2B',
+		// 		950: '#1e0c04',
+		// 	},
+		// 	red: {
+		// 		500: '#E61320',
+		// 	},
+		// 	blue: {
+		// 		50: '#E9F1F6',
+		// 		75: '#F5F9FA',
+		// 		100: '#f2f6ff',
+		// 		400: '#09B4DC',
+		// 		500: '#2F76FF',
+		// 		600: '#4C78EA',
+		// 		700: '#1c5ddf',
+		// 		800: '#082A35',
+		// 		900: '#122345',
+		// 		950: '#130c3e',
+		// 		1000: '#091329',
+		// 	},
+		// 	violet: {
+		// 		300: '#9489B6',
+		// 		400: '#9F99B2',
+		// 		500: '#6259EB',
+		// 		600: '#9452FF',
+		// 		700: '#27293D',
+		// 		800: '#1E1D33',
+		// 		850: '#181729',
+		// 		900: '#060515',
+		// 	},
+		// 	green: {
+		// 		25: '#D1FAE5',
+		// 		400: '#429D17',
+		// 		500: '#047857',
+		// 	},
+		// },
+		// fontFamily: {
+		// 	sans: ['Inter', ...defaultTheme.fontFamily.sans],
+		// 	'sans-semi': ['GeneralSans-semibold', ...defaultTheme.fontFamily.sans],
+		// 	'sans-variable': [
+		// 		'GeneralSans-Variable',
+		// 		...defaultTheme.fontFamily.sans,
+		// 	],
+		// 	'sans-medium': ['GeneralSans-medium', ...defaultTheme.fontFamily.sans],
+		// },
+		extend: {
+			// screens: {
+			// 	xxl: '1360px',
+			// 	md: '790px', // phones
+			// 	xs: '480px', // phones
+			// },
+			animation: {
+				fade: 'fadeIn .3s ease forwards',
+				fadeOut: 'fadeOut .3s ease-in-out forwards',
+				fadeScaleIn: 'fadeScaleIn .8s ease-in-out forwards',
+				rotateUp: 'rotateUp .5s ease forwards',
+				slideUp: 'slideUp .9s ease forwards',
+			},
+			keyframes: {
+				fadeIn: {
+					from: { opacity: 0 },
+					to: { opacity: 1 },
+				},
+				fadeOut: {
+					from: { opacity: 1 },
+					to: { opacity: 0 },
+				},
+				fadeScaleIn: {
+					from: { opacity: 0, transform: 'scale(1.05)' },
+					to: { opacity: 1, transform: 'scale(1)' },
+				},
+				rotateUp: {
+					from: { marginBottom: '-80px', transform: 'rotate(5deg)' },
+					to: { marginBottom: '0px', transform: 'rotate(0deg)' },
+				},
+				slideUp: {
+					from: { opacity: 0, transform: 'translateY(20px)' },
+					to: { opacity: 1, transform: 'translateY(0)' },
+				},
+			},
+			ringWidth: {
+				2.5: '2.5px',
+			},
+			// fontSize: {
+			// 	'6xl': ['3rem', '3.75rem'], // 48px 60px
+			// 	'4.5xl': ['2.5rem', '125%'],
+			// 	'2.5xl': ['1.75rem', '2.45rem'],
+			// 	base: ['1rem', '1.42rem'],
+			// 	lg: ['1.125rem', '1.6875rem'], // 18px
+			// 	sm: ['0.9375rem', '1.25rem'],
+			// 	m: ['0.9375rem', '1.5rem'],
+			// 	s: ['0.8125rem', '1.0562rem'],
+			// },
+			// lineHeight: {
+			// 	5.5: '1.42rem', // 22.72px
+			// 	5.8: '1.5rem', // 24px
+			// 	6: '1.575rem', // 25.2px
+			// 	6.5: '1.6875rem', // 27px
+			// 	9.5: '2.4rem', // 38.4px
+			// },
+			// zIndex: {
+			// 	1: '1',
+			// },
+			backgroundPosition: {
+				'center-top': 'center top',
+			},
+			// letterSpacing: {
+			// 	0.4: '-0.4px',
+			// 	0.22: '-0.22px',
+			// 	normal: '-0.22px',
+			// 	base: 'normal',
+			// 	0.56: '-0.56px',
+			// 	1.44: '-1.44px',
+			// },
+			borderRadius: {
+				'2.5xl': '20px',
+			},
+			// boxShadow: {
+			// 	'inset-3xl':
+			// 		'inset 0px -0.876389px 0.876389px 0.876389px rgba(17, 34, 69, 0.12)',
+			// 	card: '0 2px 8px -1px #130c3e0a, 0 0 0 1px #130c3e1c, 0 4px 4px -5px #130c3e40;',
+			// 	violet:
+			// 		'0 1px 2px 0 rgba(31, 18, 69, 0.08), 0 0 0 1px rgba(58, 22, 202, 0.75), 0 2px 4.3px -1px rgba(58, 22, 202, 0.4)',
+			// 	'card-use-cases': '0px 5px 30.9px -4px rgba(58, 68, 89, 0.27)',
+			// 	'card-dark': '0px 1px 7px rgba(15, 28, 53, 0.04)',
+			// 	'card-dark-contact':
+			// 		'0px 2.85px 18.335px 3.8px rgba(0, 0, 0, 0.1), inset 0px 0px 0px 1.02317px rgba(255, 255, 255, 0.04)',
+			// 	'card-dark-menu':
+			// 		'0px 11.4701px 23.7768px -5.01619px rgba(255, 255, 255, 0.07), 0px 2px 7px -1px rgba(255, 255, 255, 0.04), inset 0px 0px 0px 1px rgba(255, 255, 255, 0.12)',
+			// 	'card-orange':
+			// 		'0px 4px 4px -5px rgba(30, 12, 4, 0.25), 0px 0px 0px 1px rgba(34, 14, 5, 0.08), 0px 1px 7px rgba(30, 12, 4, 0.04)',
+			// 	'card-white':
+			// 		'0px 1px 6.3px -1px rgba(18, 35, 69, 0.1), 0px 0px 0px 1px rgba(18, 35, 69, 0.075)',
+			// 	img: '0 23.8px 30.3px 0 #0f1c3511, 0 0 0 0.8px #12234513, 0 0.8px 5px -0.8px #12234519, 0 3.2px 3.2px -4px #12234540, 0 0.8px 1.6px 0 #1223450f, 0 -2px 20.6px 0 #0000000c',
+			// 	banner:
+			// 		'0px 1px 3px rgba(18, 35, 69, 0.1), 0px 1px 2px rgba(18, 35, 69, 0.06), inset 0px 0px 0px 1px rgba(18, 35, 69, 0.1)',
+			// 	social:
+			// 		' 0px 6px 4px -2px rgba(18, 35, 69, 0.03), 0px 5.01227px 40.0982px -1px rgba(100, 109, 134, 0.06), 0px 1px 2px rgba(18, 35, 69, 0.05), 0px 0px 0px 1px rgba(217, 222, 231, 0.4)',
+			// 	'menu-item':
+			// 		'0px 2px 7px -1px rgba(15, 28, 53, 0.04),inset 0px 3px 0px -1px rgba(255, 255, 255, 0.46),inset 0px 0px 0px 1px rgba(18, 35, 69, 0.1),0px 11.47px 23.78px -5.02px rgba(18, 35, 69, 0.07)',
+			// 	video:
+			// 		'0px 11.4701px 23.7768px -5.01619px rgba(18, 35, 69, 0.07), inset 0px -0.866597px 0.866597px 0.866597px rgba(17, 34, 69, 0.12)',
+			// 	avatar:
+			// 		'0 7.3px 12.2px -3.8px rgba(18, 35, 69, 0.2), 0 16.4px 35.9px -4.9px rgba(18, 35, 69, 0.09)',
+			// },
+			// dropShadow: {
+			// 	'3xl':
+			// 		'drop-shadow(0px 0.876389px 7.01111px rgba(13, 34, 71, 0.08)) drop-shadow(0px -1.85479px 19.4753px rgba(0, 0, 0, 0.05))',
+			// },
+			spacing: {
+				4.5: '1.125rem', // 18px
+				6.5: '1.625rem', // 26px
+				7.5: '1.875rem', // 30px
+				9.5: '2.375rem', // 38px
+				13: '3.25rem', // 52px
+				15: '3.75rem', // 60px
+				15.5: '3.875rem', // 62px
+				16.5: '4.25rem', // 68px
+				17: '4.5rem', // 72px
+				17.5: '4.6875rem', // 75px
+				22: '5.625rem', // 90px
+				25: '105px', // 105px
+				30: '7.5625rem', // 121px
+				34: '8.5rem', // 136px
+			},
+			backgroundImage: {
+				main: "url('/assets/images/main-bg.png')",
+				'main-ai': "url('/assets/images/features/ai/main-bg.png')",
+				'card-right': "url('/assets/images/bg-card-right.png')",
+				'card-left-orange': "url('/assets/images/card-bg-orange.png')",
+				'card-right-blue': "url('/assets/images/card-bg-blue.png')",
+				'card-right-vivid-blue':
+					"url('/assets/images/bg-card-right-vivid-blue.png')",
+				'video-ai': "url('/assets/images/video-ai-bg.png')",
+				'card-dark-center': "url('/assets/images/bg-card-dark.png')",
+				'banner-round': "url('/assets/images/banner-bg.png')",
+				// 'main-card-gradient': "url('/assets/images/bg-card-blue.png')",
+			},
+			// opacity: {
+			// 	8: '0.08',
+			// },
+		},
+	},
 }
