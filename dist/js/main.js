652b0a1a531eb0d9dbd7a443ddfc897a9bb18353
Dmitriy <aua008@gmail.com>
2025-02-18 19:52:05 +0200

    added mobile hamburger, adaptive, fixed colors, borders and small fixes

--- dist/js/main.js
+++ dist/js/main.js
@@ -64,19 +64,8 @@ $(document).ready(function() {
 	
 	});
 
-
-	const selectWrapper = $('.selected-wrapper');	
-
-	$('.close-selected-wrapper').on('click', function(e) {
-		e.preventDefault();
-		selectWrapper.removeClass('active');
-		clearTimeout(dropdownIdTimer);
-		dropdownIdTimer = setTimeout(() => {
-			selectWrapper.css('display', 'none');
-		}, 300);
-	});
-
 	// Select files
+	const selectWrapper = $('.selected-wrapper');	
 	const selectItem = $('[data-select-item]');
 
 	selectItem.on('change', function(e) {
@@ -94,5 +83,33 @@ $(document).ready(function() {
 			}, 300);
 		}
 		selectWrapper.find('.selected-count').text(selectedItems.length);
+	});
+
+	$('.close-selected-wrapper').on('click', function(e) {
+		e.preventDefault();
+		selectWrapper.removeClass('active');
+		clearTimeout(dropdownIdTimer);
+		dropdownIdTimer = setTimeout(() => {
+			selectWrapper.css('display', 'none');
+		}, 300);
+		selectItem.prop('checked', false);
+	});
+
+
+	// Mobile search
+	function updatePlaceholder() {
+		const input = $('input[data-mobile-placeholder]');
+		if (input.length) {
+			const isMobile = window.innerWidth < 790;
+			input.attr('placeholder', isMobile ? input.data('mobilePlaceholder') : 'Search for people, Files, folder & spaces');
+		}
+	}
+	
+	window.addEventListener('resize', updatePlaceholder);
+	updatePlaceholder();
+	
+	// Mobile menu
+	$(".hamburger").on('click', function() {
+		$(this).toggleClass('is-active');
 	});
 });
