0054166af2dd1a863de614c062e80d7f89131d63
Dmitriy <aua008@gmail.com>
2025-04-10 20:19:28 +0300

    new elements have been added

--- dist/js/main.js
+++ dist/js/main.js
@@ -13,6 +13,8 @@ $(document).ready(function() {
 	const selectItem = $('[data-select-item]');
 	const titleLibrary = $('#title-library');
 	const titleLibraryTextDefault = titleLibrary.text();
+	const defaultSelectBtnCode = $('.is-select-btn').html();
+	console.log(defaultSelectBtnCode);
 	
 	selectItem.on('change', function(e) {
 		const selectedItems = selectItem.filter(':checked');
@@ -30,7 +32,7 @@ $(document).ready(function() {
 			selectWrapper.siblings('div').css('display', 'flex');
 			titleLibrary.text(titleLibraryTextDefault);
 			$('[data-content-type]').removeClass('list-select-active');
-			$('.is-select-btn').text('Select');
+			$('.is-select-btn').html(defaultSelectBtnCode);
 		}
 	});
 
@@ -43,12 +45,17 @@ $(document).ready(function() {
 		selectItem.prop('checked', false);
 		titleLibrary.text(titleLibraryTextDefault);
 		$('[data-content-type]').removeClass('list-select-active');
-		$('.is-select-btn').text('Select');
+		$('.is-select-btn').html(defaultSelectBtnCode);
 	});
 
 	$('.is-select-btn').on('click', function(e) {
 		e.preventDefault();
-		$(this).text($(this).text() === 'Cancel' ? 'Select' : 'Cancel');
+		$(this).toggleClass('active-select');
+		if($(this).hasClass('active-select')) {
+			$(this).html('Cancel');
+		} else {
+			$(this).html(defaultSelectBtnCode);
+		}
 		$('[data-content-type]').toggleClass('list-select-active');
 	});
 
