0e2e0da7604b77f79eae5fe6e32603e23c894c57
Dmitriy <aua008@gmail.com>
2025-07-18 19:03:41 +0300

    added new Billing page with adaptive, added adaptive for Settings Billing page, updated icons, fixed bugs, added Table Accordion in mobile version

---
+++ dist/js/table-accordion.js
@@ -0,0 +1,32 @@
+const TableAccordion = {
+	init: function () {
+		this.accordions = $(".table-accordion-wrapper");
+		this.initAccordion();
+	},
+	initAccordion: function () {
+		const openAccordionLinks = $(".table-accordion-open");
+		console.log(openAccordionLinks);
+
+		openAccordionLinks.on("click", function (e) {
+			e.preventDefault();
+			const wasActive = $(this).hasClass("active");
+
+			$(".table-accordion-body").slideUp(250);
+			$(".table-accordion-open").removeClass("active");
+			$(".table-accordion-icon").removeClass("-scale-100");
+
+			if (!wasActive) {
+				const wrapper = $(this).closest(".table-accordion-wrapper");
+				const body = wrapper.find(".table-accordion-body");
+				const icon = wrapper.find(".table-accordion-icon");
+				icon.addClass("-scale-100");
+				body.slideDown(250);
+				$(this).addClass("active");
+			}
+		});
+	},
+};
+
+$(document).ready(() => {
+	TableAccordion.init();
+});
